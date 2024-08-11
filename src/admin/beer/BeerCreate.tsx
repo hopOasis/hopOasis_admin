d501edb8bef6493149f63628a950298b32a3e7a4
Slava <110696118+sl7one@users.noreply.github.com>
2024-08-11 13:31:23 +0200

    fix url, fix request data fetch

--- src/admin/beer/BeerCreate.tsx
+++ src/admin/beer/BeerCreate.tsx
@@ -1,43 +1,43 @@
 import {
-	Create,
-	ImageField,
-	ImageInput,
-	TabbedForm,
-	TextInput,
-	required,
+  Create,
+  ImageField,
+  ImageInput,
+  TabbedForm,
+  TextInput,
+  required,
 } from "react-admin";
 
 export const BeerCreate = () => (
-	<Create>
-		<TabbedForm>
-			<TabbedForm.Tab label={"information"}>
-				<TextInput source="name" label="Name" validate={[required()]} />
-				<TextInput source="beerColor" validate={[required()]} />
-			</TabbedForm.Tab>
-			<TabbedForm.Tab label={"price and volume"}>
-				<TextInput source="priceSmall" validate={[required()]} />
-				<TextInput source="priceLarge" validate={[required()]} />
-				<TextInput source="volumeLarge" validate={[required()]} />
-				<TextInput source="volumeSmall" validate={[required()]} />
-			</TabbedForm.Tab>
-			<TabbedForm.Tab label={"description"}>
-				<TextInput
-					source="description"
-					variant="outlined"
-					multiline
-					fullWidth
-					validate={[required()]}
-				/>
-			</TabbedForm.Tab>
-			<TabbedForm.Tab label={"images"}>
-				<ImageInput
-					source="image"
-					accept={[".png", ".jpg"]}
-					validate={[required()]}
-				>
-					<ImageField source="src" title="title" />
-				</ImageInput>
-			</TabbedForm.Tab>
-		</TabbedForm>
-	</Create>
+  <Create>
+    <TabbedForm>
+      <TabbedForm.Tab label={"information"}>
+        <TextInput source="beerName" label="Name" validate={[required()]} />
+        <TextInput source="beerColor" validate={[required()]} />
+      </TabbedForm.Tab>
+      <TabbedForm.Tab label={"price and volume"}>
+        <TextInput source="priceSmall" validate={[required()]} />
+        <TextInput source="priceLarge" validate={[required()]} />
+        <TextInput source="volumeLarge" validate={[required()]} />
+        <TextInput source="volumeSmall" validate={[required()]} />
+      </TabbedForm.Tab>
+      <TabbedForm.Tab label={"description"}>
+        <TextInput
+          source="description"
+          variant="outlined"
+          multiline
+          fullWidth
+          validate={[required()]}
+        />
+      </TabbedForm.Tab>
+      {/* <TabbedForm.Tab label={"images"}>
+        <ImageInput
+          source="image"
+          accept={[".png", ".jpg"]}
+          validate={[required()]}
+        >
+          <ImageField source="src" title="title" />
+        </ImageInput>
+      </TabbedForm.Tab> */}
+    </TabbedForm>
+  </Create>
 );
